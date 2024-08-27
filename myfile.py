3304e62ed84991dc0eb20a972c6caf4be68ae9e6
Skanda Shastry <57039973+SkandaSShastry@users.noreply.github.com>
2024-08-27 02:39:53 +0000

    license and gitignore

--- myfile.py
+++ myfile.py
@@ -1,1 +1,10 @@
-print("hello mf")
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+number = 5
+result = factorial(number)
+
+print(f"Factorial of {nunber} is {result}")
